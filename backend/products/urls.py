ce8497d2a58db48be9099c4293af2bc488f938b1
Bogdan Milojevic <bogdanmilojevic5@gmail.com>
2025-04-27 16:42:47 +0200

    CreateAPIView

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('<int:pk>/',views.ProductDetailApiView.as_view())
+    path('<int:pk>/',views.product_detail_view),
+    path('',views.product_create_view),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -3,8 +3,21 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class=ProductSerializer
+
+    def perform_create(self,seralizer):
+        #seralizer.save(user=self.request.user)
+        print(seralizer.validated_data)
+        seralizer.save()
+
+product_create_view = ProductCreateAPIView.as_view()
+
 class ProductDetailApiView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class=ProductSerializer
+
+product_detail_view = ProductDetailApiView.as_view()
 
      
