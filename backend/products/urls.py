2f3268a36a8f82dde59476d89dfaf99c2cac6463
Bogdan Milojevic <bogdanmilojevic5@gmail.com>
2025-04-27 23:00:34 +0200

    URLs, Reverse and Serializers

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns=[
     #path('<int:pk>/',views.product_mixin_view),
-    path('<int:pk>/',views.product_detail_view),
-    path('<int:pk>/update/',views.product_update_view),
+    path('<int:pk>/',views.product_detail_view,name='product-detail'),
+    path('<int:pk>/update/',views.product_update_view,name='product-edit'),
     path('<int:pk>/delete/',views.product_destroy_view),
-    path('',views.product_list_create_view),
+    path('',views.product_list_create_view,name='product-list'),
     #path('',views.product_mixin_view),
 
     #path('<int:pk>/',views.product_ali_view),
