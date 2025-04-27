365fdf4653f90ad621cd491d82b6ccbd8b6b6eee
Bogdan Milojevic <bogdanmilojevic5@gmail.com>
2025-04-27 23:54:44 +0200

    Custom validation with serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from .models import Product
 from rest_framework.reverse import reverse
-
+from . import validators
 class ProductSerializer(serializers.ModelSerializer):
     my_discount=serializers.SerializerMethodField(read_only=True)
     edit_url=serializers.SerializerMethodField(read_only=True)
     url=serializers.HyperlinkedIdentityField(view_name='product-detail',lookup_field='pk')
 
     #email=serializers.EmailField(write_only=True)
-
+    title=serializers.CharField(validators=[validators.validate_title_no_hello,validators.validate_title])
     class Meta:
         model=Product
         fields=[
@@ -22,6 +22,13 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount',
         ]
+    
+    # def validate_title(self,value):
+    #     qs=Product.objects.filter(title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name.")
+    #     return value
+    
     
     # def create(self,validated_data):
     #     #email=validated_data.pop('email')
