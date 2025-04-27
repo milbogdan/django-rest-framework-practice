2d16e432518db30962c5eaf9374a65e56ee1e19c
Bogdan Milojevic <bogdanmilojevic5@gmail.com>
2025-04-27 18:06:36 +0200

    UpdateAPIView

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-   # path('<int:pk>/',views.product_detail_view),
-    path('<int:pk>/',views.product_ali_view),
-   # path('',views.product_list_create_view),
-    path('',views.product_ali_view),
+    path('<int:pk>/',views.product_detail_view),
+    path('<int:pk>/update/',views.product_update_view),
+    path('<int:pk>/delete/',views.product_detail_view),
+
+    path('',views.product_list_create_view),
+
+    #path('<int:pk>/',views.product_ali_view),
+    #path('',views.product_ali_view),
     #path('list/',views.product_list_view),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -22,6 +22,8 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
 product_list_create_view = ProductListCreateAPIView.as_view()
 
+
+
 class ProductDetailApiView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class=ProductSerializer
@@ -29,11 +31,31 @@ class ProductDetailApiView(generics.RetrieveAPIView):
 product_detail_view = ProductDetailApiView.as_view()
 
 
+
 class ProductListApiView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class=ProductSerializer
 
 product_list_view = ProductListApiView.as_view()
+
+
+
+class ProductUpdateApiView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class=ProductSerializer
+    lookup_field='pk'
+
+    def perform_update(self, serializer):
+        instance=serializer.save()
+        if not instance.content:
+            instance.content=instance.title
+    
+product_update_view = ProductUpdateApiView.as_view()
+
+
+
+
+
 
 @api_view(['GET','POST'])
 def product_ali_view(request, pk=None, *args, **kwargs):
