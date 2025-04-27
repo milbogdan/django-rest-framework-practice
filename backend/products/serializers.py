c1ad21440c12887cc32e46ca16d689b5d886a32d
Bogdan Milojevic <bogdanmilojevic5@gmail.com>
2025-04-27 23:28:37 +0200

    Model serializer Create and Update methods

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -6,11 +6,15 @@ class ProductSerializer(serializers.ModelSerializer):
     my_discount=serializers.SerializerMethodField(read_only=True)
     edit_url=serializers.SerializerMethodField(read_only=True)
     url=serializers.HyperlinkedIdentityField(view_name='product-detail',lookup_field='pk')
+
+    #email=serializers.EmailField(write_only=True)
+
     class Meta:
         model=Product
         fields=[
             'url',
             'edit_url',
+            #'email',
             'id',
             'title',
             'content',
@@ -19,6 +23,16 @@ class ProductSerializer(serializers.ModelSerializer):
             'my_discount',
         ]
     
+    # def create(self,validated_data):
+    #     #email=validated_data.pop('email')
+    #     obj=super().create(validated_data)
+    #     #print(email,obj)
+    #     return obj
+    
+    # def update(self, instance, validated_data):
+    #     instance.title=validated_data.get('title')
+    #     return instance
+
     def get_edit_url(self,obj):
         request = self.context.get('request')
         if request is None:
