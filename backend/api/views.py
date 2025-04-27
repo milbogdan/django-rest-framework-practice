f7f65d16ce84e9a7123c3ef196bc8101bfc08280
Bogdan Milojevic <bogdanmilojevic5@gmail.com>
2025-04-27 14:51:44 +0200

    Product model, view and serializer

---
+++ backend/api/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from products.models import Product
+from django.forms.models import model_to_dict
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
+# Create your views here.
+
+@api_view(["GET"])
+def api_home(request, *args, **kwargs):
+    instance = Product.objects.all().order_by("?").first()
+    data={}
+    if instance:
+        # data['id']=model_data.id
+        # data['title']=model_data.title
+        # data['content']=model_data.content
+        # data['price']=model_data.price
+
+        #serialization
+        #data=model_to_dict(instance,fields=['id','title','price','sale_price'])
+        data=ProductSerializer(instance).data
+    return JsonResponse(data)
+
+# def api_home(request, *args, **kwargs):
+#     print(request.GET) #print url query parameters
+#     body = request.body #byte string of json data
+#     data = {}
+#     try:
+#         data=json.loads(body) #string of json data to python dictionary
+#     except:
+#         pass
+#     print(data)
+#     data['parms']=dict(request.GET)
+#     data['headers']=dict(request.headers)
+#     data['content_type']=request.content_type
+#     return JsonResponse(data)
+
--- backend/app/settings.py
+++ backend/app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER':'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  
+        'PORT': '3306',  
     }
 }
 
