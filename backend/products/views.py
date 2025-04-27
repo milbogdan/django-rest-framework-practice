57d53eff6c97c9ba6e1279259e51115ffc95974e
Bogdan Milojevic <bogdanmilojevic5@gmail.com>
2025-04-27 18:36:41 +0200

    Mixins

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics,mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -6,6 +6,33 @@ from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
+
+
+
+class ProductMixinView(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.RetrieveModelMixin,generics.GenericAPIView):
+    queryset= Product.objects.all()
+    serializer_class=ProductSerializer
+    lookup_field='pk'
+
+
+    def get(self,request,*args,**kwargs):
+        pk=kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request,*args,**kwargs)
+        return self.list(request,*args,**kwargs)
+
+
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
+
+product_mixin_view = ProductMixinView.as_view()
+
+
+
+
+
+
+
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
